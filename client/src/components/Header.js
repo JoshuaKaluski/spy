45365c9d05aa92d6aed3bce4149403223105f358
Joshua Kaluski <joshua@kaluski.com>
2019-07-14 11:57:43 -0400

    Added logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import {ReactComponent as Logo} from '../svg/detective.svg';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    backgroundColor: '#595F72',
+    "& a": {
+      color: 'white',
+      textDecoration: 'none',
+    }
+  },
+  toolbar: {
+    justifyContent: 'space-between'
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1
   },
   title: {
     flexGrow: 1,
   },
+  buttons: {
+    flexGrow: 1,
+  }
 }));
 
 
@@ -21,11 +40,18 @@ const Header = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Spy
-          </Typography>
-          <Button color="inherit">Login</Button>
+        <Toolbar className={classes.toolbar}>
+          <Link to='/'>
+            <div className={classes.logo}>
+              <Logo/>
+              <Typography variant="h4" className={classes.title}>
+                Spy
+              </Typography>
+            </div>
+          </Link>
+          <div>
+            <Button className={classes.button} component={Link} to='/login' size="medium" color="inherit">Login</Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
