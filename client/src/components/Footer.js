934dfb359b9f2761a9fe820641c049e62a2d1b3a
Joshua Kaluski <joshua@kaluski.com>
2019-07-14 12:31:59 -0400

    Added Footer credit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper'
+
+const font = (text) => (<Typography variant='p' color='inherit'>{text}</Typography>);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+    height: '3em',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    display: 'flex',
+  }
+}));
+
+const Credit = () => {
+  return (
+    <Fragment>
+      <div>
+        {font('Icons made by ')}
+        <a href="https://www.flaticon.com/authors/itim2101" title="itim2101">
+          {font('itim2101')}
+        </a>
+        {font(' from ')}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          {font('www.flaticon.com')}
+        </a>
+        {font(' is licensed by ')}
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">
+          {font('CC 3.0 BY')}
+        </a>
+      </div>
+    </Fragment>
+  )
+};
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <div>Icons made by <a href="https://www.flaticon.com/authors/itim2101" title="itim2101">itim2101</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    <Paper className={classes.root}>
+      <Credit/>
+    </Paper>
   )
 };
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Footer from './Footer';
+
+const Landing = () => {
+  return(
+    <Fragment>
+      <Typography variant='h1'>Landing</Typography>
+      <Footer/>
+    </Fragment>
+  )
+};
+
+export default Landing;
